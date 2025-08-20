31e8924e1c704df29f84f2c6b89d82587d143529
@bhishekOjha <ojha.abhishek312@gmail.com>
2025-08-20 21:20:28 +0530

    SkillStackerCluster

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-const URL = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/resume_builder";
+const URL =
+  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/SkillStackerCluster";
 console.log("Connecting to MongoDB at:", URL);
 
 mongoose
   .connect(URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000,
   })
   .then((e) => console.log("✅ Connected to MongoDB"))
   .catch((er) => console.log("❌ MongoDB connection error:", err));
